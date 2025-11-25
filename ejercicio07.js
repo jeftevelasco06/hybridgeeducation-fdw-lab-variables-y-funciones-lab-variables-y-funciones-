6a8a7cbff7c9d4ecac2e8adc58cf47fecb40b460
jeftevelasco06 <jefte2402034@hybridge.education>
2025-11-25 11:04:56 -0600

    Add function to determine if a number is even
    Implement function to check if a number is even.

--- ejercicio07.js
+++ ejercicio07.js
@@ -1,10 +1,10 @@
 // Ejercicio 7: Comprobación de Número Par
-// Declara una función llamada "esPar" que acepte un parámetro "numero" y retorne verdadero si el número es par, de lo contrario falso
-
-//escribe tu función aquí. Recuerda que el nombre de la función debe ser exactamente "esPar"
-
+function esPar(numero) {
+    return numero % 2 === 0; // Retorna true si es par, false si es impar
+}
 
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
     esPar
 };
+
