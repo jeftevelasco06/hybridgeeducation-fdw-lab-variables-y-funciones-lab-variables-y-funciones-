1a9e816407005937539221bfe079d27b225fb034
jeftevelasco06 <jefte2402034@hybridge.education>
2025-11-25 11:06:27 -0600

    Add invertirCadena function to reverse a string
    Implement the invertirCadena function to reverse a string.

--- ejercicio10.js
+++ ejercicio10.js
@@ -1,15 +1,10 @@
 // Ejercicio 10: Inversión de una Cadena
-// Declara una función llamada "invertirCadena" que acepte un parámetro "cadena" y retorne la cadena invertida
-
-// Ejemplos:
-// invertirCadena("Hola Mundo") // debe retornar "odnuM aloH"
-// invertirCadena("anita lava la tina") // debe retornar "anit al aval atina"
-
 function invertirCadena(cadena) {
-    // Escribe tu código aquí
+    return cadena.split('').reverse().join(''); // Convierte en array, invierte y une de nuevo
 }
 
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
     invertirCadena
 };
+
