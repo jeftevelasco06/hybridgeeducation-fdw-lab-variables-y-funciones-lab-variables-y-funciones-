107421e2996a71fcb5a9a09384ac1ddaf42ef80f
jeftevelasco06 <jefte2402034@hybridge.education>
2025-11-25 11:04:28 -0600

    Add celsiusAFahrenheit function for temperature conversion
    Implemented the celsiusAFahrenheit function to convert Celsius to Fahrenheit.

--- ejercicio06.js
+++ ejercicio06.js
@@ -1,8 +1,7 @@
 // Ejercicio 6: Conversión de Grados Celsius a Fahrenheit
-// Declara una función llamada "celsiusAFahrenheit" que acepte un parámetro "celsius" y retorne la conversión a Fahrenheit
-
-//escribe tu función aquí. Recuerda que el nombre de la función debe ser exactamente "celsiusAFahrenheit"
-
+function celsiusAFahrenheit(celsius) {
+    return celsius * 9 / 5 + 32; // Aplica la fórmula de conversión
+}
 
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
