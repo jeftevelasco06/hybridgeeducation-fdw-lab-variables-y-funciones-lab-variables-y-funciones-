2fd5f5fabdcb8078b017baa31c369e216bc2c12a
jeftevelasco06 <jefte2402034@hybridge.education>
2025-11-25 11:03:26 -0600

    Update ejercicio04.js

--- ejercicio04.js
+++ ejercicio04.js
@@ -1,7 +1,7 @@
 // Ejercicio 4: Función que Saluda
-// Declara una función llamada "saludar" que acepte un parámetro "nombre" y retorne un mensaje de saludo con la estructura "Hola, nombre!".
-
-// Escribe tu función aquí. El nombre exacto debe ser saludar
+function saludar(nombre) {
+    return `Hola, ${nombre}!`; // Retorna el mensaje de saludo usando template strings
+}
 
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
