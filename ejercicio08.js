3a9de812dca602f9801707995bd17ba9bbfdc35b
jeftevelasco06 <jefte2402034@hybridge.education>
2025-11-25 11:05:20 -0600

    Add concatenarCadenas function
    Implement concatenation function for two strings

--- ejercicio08.js
+++ ejercicio08.js
@@ -1,13 +1,10 @@
 // Ejercicio 8: Concatenación de Cadenas
-// Declara una función llamada "concatenarCadenas" que acepte dos parámetros, "cadena1" y "cadena2", y retorne la concatenación de ambas cadenas
-
-//Ejemplos:
-// concatenarCadenas("Hola", "Mundo") => "HolaMundo"
-// concatenarCadenas("Frase", "de prueba") => "Frasede prueba"
-
-//escribe aquí tu función. Recuerda que el nombre de la función debe ser exactamente "concatenarCadenas"
+function concatenarCadenas(cadena1, cadena2) {
+    return cadena1 + cadena2; // Retorna la concatenación de las dos cadenas
+}
 
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
     concatenarCadenas
 };
+
